51f686a2f1219c01c2c6f2e953188266a618d937
Jay-Groen <groe0076@hz.nl>
2022-02-08 13:45:37 +0100

    Index start

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,44 +2,48 @@
 <html>
 
 <head>
-    <title>Index</title>
+    <title>Homepagina Jay Groenendijk</title>
+    <link rel="stylesheet" href="Css/myStyle.css" type="text/css" media="all">
 </head>
 
 <body>
 <div>
     <nav>
         <ul class="navbar sticky">
-            <li> <a href="http://localhost/">Home</a></li>
-            <li> <a href="http://localhost/profile.html"> Profile</a> </li>
-            <li><a href="http://localhost/dashboard.html"> Dashboard</a></li>
-            <li> <a href="http://localhost/faq.html"> FAQ</a></li>
-            <li><a href="http://localhost/postfeed.html"> Postfeed</a></li>
+            <li> <a href="/">Home</a></li>
+            <li> <a href="/profile.html"> Profile</a> </li>
+            <li> <a href="/dashboard.html"> Dashboard</a></li>
+            <li> <a href="/faq.html"> FAQ</a></li>
+            <li> <a href="/postfeed.html"> Blog</a></li>
+            <img class="hz-image"
+                 src="https://th.bing.com/th/id/OIP.VuOx_zmNq-m0TBZqw2vuwwHaHa?w=178&h=180&c=7&r=0&o=5&dpr=1.5&pid=1.7">
+            </img>
         </ul>
     </nav>
 </div>
 <main>
-    <h1> </h1>
 </main>
-<img class="hz-image"
-     src="https://th.bing.com/th/id/OIP.VuOx_zmNq-m0TBZqw2vuwwHaHa?w=178&h=180&c=7&r=0&o=5&dpr=1.5&pid=1.7">
-</img>
-<p>
-<h2>
-    HOME PAGE VAN JAY GROENENDIJK
-</h2>
-</p>
-<p>
-<div id="home-page">
-    <h3>
-        Waarom ik bij de opleiding pas!
-    </h3>
-    <ul class="waarom-ict">
-        <li>Ik vind werken met computers leuk.</li>
-        <li>Mijn vader doet ict werk.</li>
-        <li></li>
-    </ul>
+<div class="homepage-tekst">
+    <h1>
+        Homepage van <br> Jay Groenendijk
+    </h1>
+    <p>
+        Student aan de HBO ICT-opleiding op <a href="https://hz.nl/">HZ University Of Applied Sciences</a>
+    </p>
 </div>
-</p>
+<div>
+    <img class="hz-gebouw" src="img/hz_middelburg_foto_1.jpg">
+</div>
+<div class="waarom-ik-bij-de-opleiding-pas">
+    <h3>
+        Waarom ik bij de opleiding pas
+    </h3>
+    <p>
+        Ik vind het leuk om te werken met computers en ben erg geïnteresseerd in programmeren. Vroeger wilde ik
+        ook graag game developer worden. Ik ben erg nieuwsgierig en hoop dat te kunnen toepassen op deze opleiding.
+        Ik vind het ook leuk om samen met anderen aan een project te werken.
+    </p>
+</div>
 
 </body>
 
